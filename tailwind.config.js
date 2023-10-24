29992d5b616ec0269059bf96b6016b174c70f2bd
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2023-10-24 21:11:46 +0000

    Se entido el Porque del Witch

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
       darkMode: false, // or 'media' or 'class'
       theme: {
         extend: {
-         
+         backgroundImage : theme => ({
+          'wave-pattern' : 'url(../img/wave-white.png)'
+         })
         }
       },
       variants: {
